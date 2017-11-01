150dfe57abab209e38887f22bce6a088910a6b80
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-11-01 20:40:21 +0530

    changed the timing for the add to homescreen prompt.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,17 @@
+var deferredPrompt;
+
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register("/sw.js").then(() => {
         console.log("Service Worker registered.");
     });
 }
+
+//Changing the time when add to homescreen appears
+//Disabling the default timing and saving the prompt
+//in a deferredPrompt variable
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    console.log("beforeinstallprompt fired");
+    deferredPrompt = prompt;
+    return false;
+});
--- public/src/js/log.js
+++ public/src/js/log.js
@@ -11,6 +11,23 @@ function toggleNewLogView(e) {
         newLogButton.className = "new-modal";
         newLogModal.style.display = "none";
     }
+
+    //Checking if deferredPrompt is there
+    //Then show the install to homescreen
+    //after clicking the + button
+    //after 1 sec
+    if(deferredPrompt) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            console.log(choiceResult.outcome);
+            if(choiceResult.outcome === "dismissed") {
+                console.log("User dismissed the prompt.");
+            } else {
+                console.log("User added to the homescreen.");
+            }
+        });
+        deferredPrompt = null;
+    }
 }
 
 newLogButton.addEventListener("click", toggleNewLogView);
