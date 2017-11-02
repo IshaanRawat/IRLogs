ecb61ba8d2a628cfe37e24d1803780c6dc40161c
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-11-02 16:43:36 +0530

    Removed unwanted code

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -19,30 +19,3 @@ window.addEventListener("beforeinstallprompt", (event) => {
     deferredPrompt = prompt;
     return false;
 });
-
-//FETCH API for GET
-fetch("https://httpbin.org/ip").then((response) => {
-    console.log(response);
-    return response.json();
-}).then((data) => {
-    console.log(data);  
-}).catch((err) => {
-    console.log(err);
-});
-
-//FETCH API for POST
-fetch("https://httpbin.org/post", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify({code: "Hey there"})
-}).then((response) => {
-    console.log(response);
-    return response.json();
-}).then((data) => {
-    console.log(data);  
-}).catch((err) => {
-    console.log(err);
-});
