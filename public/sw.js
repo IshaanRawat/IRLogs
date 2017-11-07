fd3bff43c04eceed628f2eace7578d3b27723424
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-11-07 14:12:37 +0530

    added files to cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,24 @@
 self.addEventListener("install", (event) => {
     console.log("[Service Worker] Installing Service Worker...", event);
+    event.waitUntil(
+        caches.open("static")
+            .then((cache) => {
+                console.log("[Service Worker] Precaching App Shell ...");
+                cache.addAll([
+                    "/",
+                    "/index.html",
+                    "/src/js/app.js",
+                    "/src/js/log.js",
+                    "/src/js/promise.js",
+                    "/src/js/fetch.js",
+                    "/src/css/style.css",
+                    "/src/css/backpack.css",
+                    "/src/media/marker.svg",
+                    "/src/media/ir.svg",
+                    "https://fonts.googleapis.com/css?family=Nunito:400,600,900"
+                ]);
+            })
+    );
 });
 
 self.addEventListener("activate", (event) => {
@@ -9,5 +28,14 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
     // console.log("[Service Worker] Fetching something...", event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if(response) {
+                    return response;
+                } else {
+                    return event.request;
+                }
+            })
+    );
 });
