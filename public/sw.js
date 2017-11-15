e7604c5b7f8117d4e79d3aed97b27fd162d23e24
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-11-15 10:04:46 +0530

    separated the pages to be cached in a variable

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,18 @@
 var CACHE_STATIC_VERSION = "static-v3";
 var CACHE_DYNAMIC_VERSION = "dynamic-v2";
+var cachedPages = [
+    "/",
+    "/index.html",
+    "/src/js/app.js",
+    "/src/js/log.js",
+    "/src/js/promise.js",
+    "/src/js/fetch.js",
+    "/src/css/style.css",
+    "/src/css/backpack.css",
+    "/src/media/marker.svg",
+    "/src/media/ir.svg",
+    "https://fonts.googleapis.com/css?family=Nunito:400,600,900"
+];
 
 self.addEventListener("install", (event) => {
     console.log("[Service Worker] Installing Service Worker...", event);
@@ -7,19 +20,7 @@ self.addEventListener("install", (event) => {
         caches.open(CACHE_STATIC_VERSION)
             .then((cache) => {
                 console.log("[Service Worker] Precaching App Shell ...");
-                cache.addAll([
-                    "/",
-                    "/index.html",
-                    "/src/js/app.js",
-                    "/src/js/log.js",
-                    "/src/js/promise.js",
-                    "/src/js/fetch.js",
-                    "/src/css/style.css",
-                    "/src/css/backpack.css",
-                    "/src/media/marker.svg",
-                    "/src/media/ir.svg",
-                    "https://fonts.googleapis.com/css?family=Nunito:400,600,900"
-                ]);
+                cache.addAll(cachedPages);
             })
     );
 });
