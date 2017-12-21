24ef62dcb9a2a0c73e36d4b962255d27aaa0df2c
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-12-21 20:33:31 +0530

    created a storeLogsData firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.storeLogsData = functions.https.onRequest((request, response) => {
+    cors((request, response) => {
+        admin.database().ref('posts').push({
+            id: request.body.id,
+            title: request.body.title,
+            location: request.body.title,
+            image: request.body.image
+        })
+        .then(() => {
+            response.status(201).json({message: "Data stored.", id: request.body.id});
+        })
+        .catch((err) => {
+            response.status(500).json({error: err});
+        });
+    });
+});
