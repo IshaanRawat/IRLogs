9ef4d75ab2f5a95558608c40231148434f75a014
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-12-22 22:39:50 +0530

    authenticated and fixed the cors bug

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,16 @@ const cors = require('cors')({origin: true});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+var serviceAccount = require("./firebase-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://irlogs-f4861.firebaseio.com"
+});
+
 exports.storeLogsData = functions.https.onRequest((request, response) => {
-    cors(function(request, response) {
+    cors(request, response, function() {
         admin.database().ref('posts').push({
             id: request.body.id,
             title: request.body.title,
