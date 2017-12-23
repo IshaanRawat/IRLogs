3aeefb054f44837caf4dcc41fd9cf65d7b6c6e62
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-12-23 15:48:27 +0530

    added a notification click event in serviceWorker

--- public/sw.js
+++ public/sw.js
@@ -136,3 +136,16 @@ self.addEventListener("sync", (event) => {
         );
     }
 });
+
+self.addEventListener("notificationclick", (event) => {
+    const notification = event.notification;
+    const action = event.action;
+    console.log(notification);
+
+    if(action === "confirm") {
+        console.log("Confirm was chosen.");
+        notification.close();
+    } else {
+        console.log(action);
+    }
+})
