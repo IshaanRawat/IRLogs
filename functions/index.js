e7690857ac189ef28b09f287e5cf86e192e7fe89
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-12-25 11:17:44 +0530

    configured backend with push messages on new post addition

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
+const webpush = require("web-push");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -22,6 +23,25 @@ exports.storeLogsData = functions.https.onRequest((request, response) => {
             image: request.body.image
         })
         .then(() => {
+            const vapidPrivateKey = "jfijteP7yQR89sveCsBJfQ1qW5BTARzudRJlZ2PKAtY";
+            const vapidPublicKey = "BBTlbc29fuuQW8m18NEZ01AK2rl_IpgES37cYzGGwO0gTKCoozliJNhkAbHcMqcTg-hFnrv-77-BwBxktOyn-bE";
+            webpush.setVapidDetails("mailto:support@ishaanrawat.com", vapidPublicKey, vapidPrivateKey);
+            return admin.database().ref("subscriptions").once("value");
+        })
+        .then((subscriptions) => {
+            subscriptions.forEach((sub) => {
+                let pushConfig = {
+                    endpoint: sub.val().endpoint,
+                    keys: {
+                        auth: sub.val().keys.auth,
+                        p256dh: sub.val().keys.p256dh
+                    }
+                };
+                webpush.sendNotification(pushConfig, JSON.stringify({title: "New Post", content: "New Post Added!"}))
+                .catch((err) => {
+                    console.log(err);
+                });
+            });
             response.status(201).json({message: "Data stored.", id: request.body.id});
         })
         .catch((err) => {
